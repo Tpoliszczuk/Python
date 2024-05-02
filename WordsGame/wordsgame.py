a9f0d7712231cb8ba7b642ae9386edf3b6ae5f70
unknown <tpoliszczuk@outlook.com>
2024-05-02 20:46:32 +0200

    new wordsgame layout

--- WordsGame/wordsgame.py
+++ WordsGame/wordsgame.py
@@ -5,16 +5,14 @@ import ctypes
 import psycopg2
 from time import sleep
 import threading
-
-
-
+import customtkinter as ctk
 
 # Connection to Postgres DB
 credentials = {
     "host": "localhost",
     "database": "postgres",
     "user": "postgres",
-    "password": "password"
+    "password": "xxxx"
 }
 current_word_index = 0
 
@@ -30,7 +28,6 @@ class TreeviewEdit(ttk.Treeview):
         if region_clicked not in ("cell"):
             return
         
-        #Który item klikamy
         column=self.identify_column(event.x)
 
         column_index=int(column[1:])-1
@@ -38,9 +35,7 @@ class TreeviewEdit(ttk.Treeview):
         selected_values=self.item(selected_iid)
         selected_text=selected_values.get("values")[column_index]
         
-        
         column_box=self.bbox(selected_iid,column)
-
 
         entry_edit=ttk.Entry(frame_word_list,width=column_box[2])
         entry_edit.editing_column_index=column_index
@@ -68,7 +63,6 @@ class TreeviewEdit(ttk.Treeview):
         current_values=self.item(selected_iid).get("values")
         
         current_values[column_index]=new_text
-        print(current_values)
         update_db(current_values)
         self.item(selected_iid,values=current_values)
 
@@ -76,9 +70,7 @@ class TreeviewEdit(ttk.Treeview):
         event.widget.destroy()
 
     def on_delete(self,event):
-         #Który item klikamy
         column=self.identify_column(event.x)
-
         column_index=int(column[1:])-1
         selected_iid=self.focus()
         selected_values=self.item(selected_iid)
@@ -94,7 +86,6 @@ def delete_db(current_values):
     cursor = conn.cursor()
     try:
         query = f"delete from wordsgame where id={record_id}"
-        print(query)
         cursor.execute(query)
         conn.commit()
         cursor.close()
@@ -168,8 +159,8 @@ def show_word_list_screen():
     frame_word_list_menu.pack_forget()
     frame_main.pack_forget() 
     frame_add_word.pack_forget() 
-    frame_word_list.pack()
-    frame_word_list_menu.pack()  
+    frame_word_list.pack(expand=True, fill='both')
+    frame_word_list_menu.pack(expand=True, fill='both')
     refresh_word_list() 
 
 #Update records in DB
@@ -184,7 +175,7 @@ def update_word(word_id, word, meaning, pinyin):
     update_user_word(conn, word_id, word, meaning, pinyin)
 
 
-#Refresh table with words list
+#Refresh table words_list
 def refresh_word_list():    
     for child in frame_word_list.get_children():
         frame_word_list.delete(child)
@@ -196,14 +187,9 @@ def refresh_word_list():
     cursor.close()
     for record in words:
         id, word,meaning, pinyin = record
-        print(f"{word},{meaning},{pinyin}")
         frame_word_list.insert(parent="",
                         index=tk.END,
                         values=(f"{word}",f"{meaning}",f"{pinyin}",f"{id}"))
-
-    
-
-
 
 
 def start_learning():
@@ -212,25 +198,25 @@ def start_learning():
     words = get_user_words(conn, username)
     current_word_index = 0
     frame_menu.pack_forget()
-    frame_main.pack()
+    frame_main.pack(expand=True, fill='both')
     display_random_word()
 
 #Add word button
 def show_add_word_screen():
     frame_menu.pack_forget()
-    frame_add_word.pack()
+    frame_add_word.pack(expand=True, fill='both')
 
 def add_word_to_database():
     han = entry_han.get()
     if han == '':
-         messagebox.showerror("Błąd", "Muszisz dodać słowo")
+         messagebox.showerror("Error", "Uou need to add word")
          return 
     pinyin = entry_pinyin.get()
     znaczenie = entry_znaczenie.get()
     add_word(conn, username, han, znaczenie, pinyin)
-    messagebox.showinfo("Sukces", "Słowo zostało dodane!")
+    messagebox.showinfo("Sucess", "Word added!")
     frame_add_word.pack_forget()
-    frame_menu.pack()
+    frame_menu.pack(expand=True, fill='both')
 
 def add_word(conn, user_id, han, znaczenie, pinyin):
     cursor = conn.cursor()
@@ -244,7 +230,7 @@ def back_to_menu():
     frame_word_list.pack_forget()
     frame_word_list_menu.pack_forget()
     frame_main.pack_forget()
-    frame_menu.pack()
+    frame_menu.pack(expand=True, fill='both')
 
 
 def handle_login():
@@ -254,7 +240,7 @@ def handle_login():
     user = login(conn, username, password)
     if user:
         frame_login.pack_forget()
-        frame_menu.pack()
+        frame_menu.pack(expand=True, fill='both')
     else:
         messagebox.showerror("Login Error", "Wrong login credentials")
 
@@ -262,8 +248,11 @@ def handle_login():
 def handle_registration():
     username = entry_username.get()
     password = entry_password.get()
-    register(conn, username, password)
-    messagebox.showinfo("Success", "Your account has been created. Now you can log in")
+    if username =="" or password=="":
+        messagebox.showerror("Error", "You need to write username and password ,then click register")
+    else:
+        register(conn, username, password)
+        messagebox.showinfo("Success", "Your account has been created. Now you can log in")
 
 
 def display_meaning():
@@ -291,123 +280,126 @@ def display_random_word():
     else:
         messagebox.showinfo("Thats all", "There are no more words to learn!")
         frame_main.pack_forget()
-        frame_menu.pack()
+        frame_menu.pack(expand=True, fill='both')
 
 # Function to display pinyin after delay
 def show_pinyin_after_delay(pinyin):
-    label_pinyin.config(text=pinyin)
-
-
-def update_width(event):
-    global window_width
-    window_width = event.width
-    root.after(100, update_elements)
-
-
-def update_elements():
-    entry_username.config(width=int(window_width/8))
-    entry_password.config(width=int(window_width/8))
+    try:
+        label_pinyin.config(text=pinyin)
+    except:
+        pass
 
 
 
 conn = connect_to_database(credentials)
 
 
+def set_window_position(window, width, height):
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    x = (screen_width - width) // 2
+    y = (screen_height - height) // 2
+    root.geometry(f"{width}x{height}+{x}+{y}")
 
 
-if __name__=="__main__":
-
+if __name__ == "__main__":
+    ctk.set_appearance_mode('system')
+    ctk.set_default_color_theme('dark-blue')
     root = tk.Tk()
     root.title("Flashcards APP")
-    s=ttk.Style(root)
-    s.theme_use("clam")
+    root.minsize(300, 200)
     window_width = 600
     window_height = 600
     set_window_position(root, window_width, window_height)
 
-
-
-    #Login screen frame
+    # Login screen frame
     frame_login = tk.Frame(root)
-    frame_login.columnconfigure(0,weight=1)
-    frame_login.columnconfigure(1,weight=1)
-    frame_login.rowconfigure(0,weight=1)
-    frame_login.rowconfigure(1,weight=1)
-    frame_login.rowconfigure(2,weight=1)
-    frame_login.rowconfigure(3,weight=1)
-    
-    label_username = tk.Label(frame_login, text="User:")
-    label_username.grid(row=0, column=0)
-    entry_username = tk.Entry(frame_login,width=int(window_width/8),)
-    entry_username.grid(row=0, column=1)
-    label_password = tk.Label(frame_login, text="Password:")
-    label_password.grid(row=1, column=0, padx=5, pady=20)
-    entry_password = tk.Entry(frame_login, show="*", width=int(window_width/8),)
-    entry_password.grid(row=1, column=1, padx=10, pady=20)
-    button_login = tk.Button(frame_login, text="Login", width=int(window_width/8), command=handle_login)
-    button_login.grid(row=2, column=0, columnspan=2, padx=5, pady=10)
-    button_register = tk.Button(frame_login, text="Register", width=int(window_width/8), command=handle_registration)
-    button_register.grid(row=3, column=0, columnspan=2, padx=5, pady=10)
-    frame_login.pack(padx=10, pady=10)
+    frame_login.pack(expand=True, fill='both')
 
+    frame_login.columnconfigure((0, 3), weight=2)
+    frame_login.columnconfigure((1, 2), weight=1)
+    frame_login.rowconfigure((0, 5), weight=10)
+    frame_login.rowconfigure((1, 2, 3, 4), weight=1)
 
+    username_label = ctk.CTkLabel(frame_login, text='Username', font=('Arial', 18))
+    username_label.grid(row=1, column=1)
+    entry_username = ctk.CTkEntry(frame_login)
+    entry_username.grid(row=1, column=2, sticky='ew', padx=10, pady=20)
 
+    password_label = ctk.CTkLabel(frame_login, text='Password', font=('Arial', 18))
+    password_label.grid(row=2, column=1)
+    entry_password = ctk.CTkEntry(frame_login, show='*')
+    entry_password.grid(row=2, column=2, sticky='ew', padx=10, pady=10)
 
-    #Added window resizing event
-    root.bind("<Configure>", update_width)
+    login_button = tk.Button(frame_login, text='Login', font=('Arial', 18), command=handle_login)
+    login_button.grid(row=3, column=1, columnspan=2, sticky='ew', padx=10, pady=10)
 
+    register_button =  tk.Button(frame_login, text='Register', font=('Arial', 18), command=handle_registration)
+    register_button.grid(row=4, column=1, columnspan=2, sticky='ew', padx=10, pady=10)
 
     #Main Manu Frame
     frame_menu = tk.Frame(root)
-    button_learn = tk.Button(frame_menu, text="Learn", width=int(window_width/16), command=start_learning)
-    button_learn.pack(pady=10)
-    button_add_words = tk.Button(frame_menu, text="Add words", width=int(window_width/16), command=show_add_word_screen)
-    button_add_words.pack(pady=10)
-    button_word_list = tk.Button(frame_menu, text="Word list", width=int(window_width/16), command=show_word_list_screen)
-    button_word_list.pack(pady=10)
+
+    frame_menu.columnconfigure((0, 3), weight=2)
+    frame_menu.columnconfigure((1, 2), weight=1)
+    frame_menu.rowconfigure((0, 5), weight=10)
+    frame_menu.rowconfigure((1, 2, 3, 4), weight=1)
+
+    button_learn = tk.Button(frame_menu, text="Learn", font=('Arial', 18), command=start_learning)
+    button_learn.grid(row=1, column=1,columnspan=2, sticky='ew', padx=10, pady=10)
+    button_add_words = tk.Button(frame_menu, text="Add words", font=('Arial', 18), command=show_add_word_screen)
+    button_add_words.grid(row=2, column=1,columnspan=2, sticky='ew', padx=10, pady=10)
+    button_word_list = tk.Button(frame_menu, text="Word list", font=('Arial', 18), command=show_word_list_screen)
+    button_word_list.grid(row=3, column=1,columnspan=2, sticky='ew', padx=10, pady=10)
 
     #Add words Frame
     frame_add_word = tk.Frame(root)
-    label_han = tk.Label(frame_add_word, text="Word:", width=int(window_width/16))
-    label_han.grid(row=0, column=0, padx=5, pady=5)
-    entry_han = tk.Entry(frame_add_word, width=int(window_width/16))
-    entry_han.grid(row=0, column=1, padx=5, pady=5)
-    label_pinyin = tk.Label(frame_add_word, text="Pinyin:", width=int(window_width/16))
-    label_pinyin.grid(row=1, column=0, padx=5, pady=5)
-    entry_pinyin = tk.Entry(frame_add_word, width=int(window_width/16))
-    entry_pinyin.grid(row=1, column=1, padx=5, pady=5)
-    label_znaczenie = tk.Label(frame_add_word, text="Meaning:", width=int(window_width/16))
-    label_znaczenie.grid(row=2, column=0, padx=5, pady=5)
-    entry_znaczenie = tk.Entry(frame_add_word, width=int(window_width/16))
-    entry_znaczenie.grid(row=2, column=1, padx=5, pady=5)
-    button_add = tk.Button(frame_add_word, text="Add Word", width=int(window_width/16), command=add_word_to_database)
-    button_add.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
-    button_back = tk.Button(frame_add_word, text="Back to menu", width=int(window_width/16), command=back_to_menu)
-    button_back.grid(row=4, column=0, columnspan=2, padx=5, pady=5)
+    frame_add_word.columnconfigure((0, 3), weight=2)
+    frame_add_word.columnconfigure((1, 2), weight=1)
+    frame_add_word.rowconfigure((0, 5), weight=10)
+    frame_add_word.rowconfigure((1, 2, 3, 4), weight=1)
+
+    label_han = tk.Label(frame_add_word, text="Word:", font=('Arial', 18))
+    label_han.grid(row=1, column=1, padx=5, pady=5)
+    entry_han = tk.Entry(frame_add_word, font=('Arial', 18))
+    entry_han.grid(row=1, column=2, padx=5, pady=5)
+    label_pinyin = tk.Label(frame_add_word, text="Pinyin:", font=('Arial', 18))
+    label_pinyin.grid(row=2, column=1, padx=5, pady=5)
+    entry_pinyin = tk.Entry(frame_add_word, font=('Arial', 18))
+    entry_pinyin.grid(row=2, column=2, padx=5, pady=5)
+    label_znaczenie = tk.Label(frame_add_word, text="Meaning:", font=('Arial', 18))
+    label_znaczenie.grid(row=3, column=1, padx=5, pady=5)
+    entry_znaczenie = tk.Entry(frame_add_word, font=('Arial', 18))
+    entry_znaczenie.grid(row=3, column=2, padx=5, pady=5)
+    button_add = tk.Button(frame_add_word, text="Add Word", font=('Arial', 18),  command=add_word_to_database)
+    button_add.grid(row=4, column=1, columnspan=2, padx=5, pady=10)
+    button_back = tk.Button(frame_add_word, text="Back to menu", font=('Arial', 18), command=back_to_menu)
+    button_back.grid(row=5, column=1, columnspan=2, padx=5, pady=5)
 
     #Learn Frame
     frame_main = tk.Frame(root)
-    label_han = tk.Label(frame_main, font=("Helvetica", 24))
-    label_han.pack(pady=20)
-    label_pinyin = tk.Label(frame_main, font=("Helvetica", 16))
-    label_pinyin.pack(pady=10)
-    label_znaczenie = tk.Label(frame_main, font=("Helvetica", 16))
-    label_znaczenie.pack(pady=10)
 
-    frame_buttons = tk.Frame(frame_main)
-    button_meaning = tk.Button(frame_buttons, text="Meaning", width=int(window_width/16), command=display_meaning)
-    button_meaning.pack(side=tk.LEFT, padx=10)
-    button_next = tk.Button(frame_buttons, text="Next", width=int(window_width/16), command=display_random_word)
-    button_next.pack(side=tk.LEFT, padx=10)
-    frame_buttons.pack()
+    frame_main.columnconfigure((0, 3), weight=2)
+    frame_main.columnconfigure((1, 2), weight=1)
+    frame_main.rowconfigure((0), weight=6)
+    frame_main.rowconfigure((1,2,3), weight=2)
+    frame_main.rowconfigure(( 4,5,6,7), weight=1)
 
-    button_back = tk.Button(frame_main, text="Back to menu", width=30, command=back_to_menu)
-    button_back.pack(pady=20)
+    label_han = tk.Label(frame_main, font=("Helvetica", 36))
+    label_han.grid(row=1, column=1,columnspan=2)
+    label_pinyin = tk.Label(frame_main, font=("Helvetica", 36))
+    label_pinyin.grid(row=2, column=1,columnspan=2)
+    label_znaczenie = tk.Label(frame_main, font=("Helvetica", 36))
+    label_znaczenie.grid(row=3, column=1,columnspan=2)
 
 
-
-
-
+    button_meaning = tk.Button(frame_main, text="Meaning", font=('Arial', 18), command=display_meaning)
+    button_meaning.grid(row=5, column=1,columnspan=2,pady=20,sticky="ew")
+    button_next = tk.Button(frame_main, text="Next",  font=('Arial', 18), command=display_random_word)
+    button_next.grid(row=6, column=1,columnspan=2,pady=20,sticky="ew")
+ 
+    button_back = tk.Button(frame_main, text="Back to menu", font=('Arial', 18), command=back_to_menu)
+    button_back.grid(row=7, column=1,columnspan=2,pady=20)
 
 
     #Words List Frame
@@ -421,6 +413,9 @@ if __name__=="__main__":
     # Hide default column '#0'
     frame_word_list.column("id", width=0, stretch=False)
     frame_word_list.column("#0", width=0, stretch=False)
+    frame_word_list.column("word", anchor=tk.CENTER)    
+    frame_word_list.column("meaning", anchor=tk.CENTER)  
+    frame_word_list.column("pinyin", anchor=tk.CENTER) 
 
     frame_word_list_menu=tk.Frame(root)
     frame_word_list_menu = tk.Frame(root)
@@ -432,10 +427,11 @@ if __name__=="__main__":
     frame_word_list_menu.rowconfigure(3,weight=1)
     
 
-    button_register = tk.Button(frame_word_list_menu, text="Back to menu", width=30, command=back_to_menu)
-    button_register.grid(row=3, column=1, columnspan=1, padx=5, pady=10)
+    button_register = tk.Button(frame_word_list_menu, text="Back to menu",font=('Arial', 18), command=back_to_menu)
+    button_register.grid(row=3, column=0, columnspan=2, padx=5, pady=10)
     frame_login.pack(padx=10, pady=10)
-
+    style = ttk.Style()
+    style.configure("Treeview.Heading", font=('Helvetica', 12, 'bold')) 
 
     #Mainloop start
     root.mainloop()
